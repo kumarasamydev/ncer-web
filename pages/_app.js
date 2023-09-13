0aee0aeecf2d378050ace41b36adea83e5f1601a
kumarasamy nagaraj <writetokumarasamy@gmail.com>
2023-09-13 13:15:54 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-import '@/styles/globals.css'
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Component {...pageProps} />
+    </div>
+  );
 }
